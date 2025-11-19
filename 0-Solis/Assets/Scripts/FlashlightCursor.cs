799e11cda70f52fef5b6ce8c219599a551a572a9
Darmatage <JasonWiserArt@gmail.com>
2025-11-19 14:07:49 -0500

    Flashlight system enabled-- needs cone

--- 0-Solis/Assets/Scripts/FlashlightCursor.cs
+++ 0-Solis/Assets/Scripts/FlashlightCursor.cs
@@ -1,22 +1,79 @@
+using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class FlashlightCursor : MonoBehaviour
 {
-    public Camera cam;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //public Camera cam;
+
+	public Transform flashlight;
+	public static float distanceMultiplier;
+	public float distanceMultiplierStart = 6;
+
+//from deathgaze: which can we remov?
+	public float rotationSpeed = 30;
+       public float distance = 50;
+       public LineRenderer lineOfSight;
+       public Gradient redColor;
+       public Gradient greenColor;
+       //public GameObject hitEffectAnim;
+
+       public int EnemyLives = 30;
+       private Renderer rend;
+	bool lightsOn = false;
+
     void Start()
     {
-        cam = Camera.main;
+		distanceMultiplier = distanceMultiplierStart;
+		Physics2D.queriesStartInColliders = false;
+		lineOfSight.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        Vector3 mousePos = (Vector2)cam.ScreenToWorldPoint(Input.mousePosition);
-        float angleRad = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x);
-        float angleDeg = (180/Mathf.PI) * angleRad - 90; 
 
-        transform.rotation = Quaternion.Euler(0f, 0f, angleDeg);
-        Debug.DrawLine(transform.position, mousePos, Color.yellow, Time.deltaTime);
-    }
+		if (Input.GetKeyDown("f"))
+		{
+			lightsOn = !lightsOn;
+		}
+
+		Vector2 direction = (flashlight.position - transform.position).normalized;
+		//distanceMultiplier = 6f; // Example distance
+    	Vector3 offsetVector = direction * distanceMultiplier;
+		Vector2 lightDistance = flashlight.position + offsetVector;
+
+		//Debug.DrawLine(flashlight.position, lightDistance, Color.yellow, Time.deltaTime);
+
+
+		// Raycast needs location, Direction, and length
+		RaycastHit2D hitInfo = Physics2D.Raycast (flashlight.position, direction, distance);
+	if (lightsOn){	
+		lineOfSight.gameObject.SetActive(true);
+		if (hitInfo.collider != null) {
+			Debug.DrawLine(flashlight.position, hitInfo.point, Color.red);
+			lineOfSight.SetPosition(1, hitInfo.point); // index 1 is the end-point of the line
+			lineOfSight.colorGradient = redColor;
+
+			if (hitInfo.collider.CompareTag ("Enemy")) {
+				//make enemy move back
+				//hitInfo.collider.gameObject.GetComponent<EnemyMove>().HitByLight();
+				Debug.Log ("I hit an enemy: " + hitInfo.collider.gameObject.name);
+			}
+		} else {
+			Debug.DrawLine(flashlight.position, lightDistance, Color.green);
+			lineOfSight.SetPosition(1, lightDistance);
+			lineOfSight.colorGradient = greenColor;
+		}
+		
+		lineOfSight.SetPosition (0, flashlight.position); // index 0 is the start-point of the line
+	} 
+	else
+		{
+			lineOfSight.gameObject.SetActive(false);
+		}  
+	   
+	}
+
+
 }
