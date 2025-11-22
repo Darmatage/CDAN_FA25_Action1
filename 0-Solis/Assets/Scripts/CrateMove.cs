d5905be7976b8c377aa58097c44e4a4233e86e54
djbianchino-sketch <djbianchino@massart.edu>
2025-11-21 21:43:40 -0500

    messed around with crate

--- 0-Solis/Assets/Scripts/CrateMove.cs
+++ 0-Solis/Assets/Scripts/CrateMove.cs
@@ -1,111 +1,118 @@
-using System.Collections.Generic;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CrateMove : MonoBehaviour {
 
-	public LayerMask groundLayer;
-	public Transform crateBottom;
-	float groundRange = 0.1f;
-	public bool isGrounded = false;
+    [Header("Ground Check")]
+    public LayerMask groundLayer;
+    public Transform crateBottom;
+    float groundRange = 0.1f;
+    public bool isGrounded = false;
 
-	//For pulling crate, create a joint:
-	GameObject thePlayer;
-	FixedJoint2D fixedJoint;
-	public bool isTouchingPlayer = false;
-	public bool canPull = false;
+    [Header("Dragging")]
+    public AudioSource[] dragSounds;
+    public float minPitch = 0.95f;
+    public float maxPitch = 1.05f;
+    public float dragRepeatTime = 0.5f; // delay between sounds
 
-	public Transform jointNode;
+    private AudioSource currentDragSound;
+    private float dragTimer = 0f;
+    private Vector3 lastColliderPos;
 
-	void Update()
-	{
+    //For pulling crate, create a joint:
+    GameObject thePlayer;
+    FixedJoint2D fixedJoint;
+    public bool isTouchingPlayer = false;
+    public bool canPull = false;
+    public Transform jointNode;
 
-		if (Input.GetKeyDown("e")){
-			if (isTouchingPlayer && fixedJoint == null){
-				jointNode.position = thePlayer.transform.position;
-				jointNode.parent = thePlayer.transform;
-				gameObject.transform.parent = jointNode;
+    void Start() {
+        lastColliderPos = crateBottom.position;
+    }
 
-				fixedJoint = jointNode.gameObject.AddComponent<FixedJoint2D>();
-				fixedJoint.connectedBody = thePlayer.GetComponent<Rigidbody2D>();
-				fixedJoint.autoConfigureConnectedAnchor=false; 
-				canPull = true;
-			}
-			else if (fixedJoint != null)
-			{
-				// Release the crate
-				Destroy(fixedJoint);
-				thePlayer = null;
-				canPull = false;
-				gameObject.transform.parent = null;
-				jointNode.parent = gameObject.transform;
-				
-			}
-		}
+    void Update() {
+    // Handle pulling/releasing
+    if (Input.GetKeyDown("e")) {
+        if (isTouchingPlayer && fixedJoint == null) {
+            jointNode.position = thePlayer.transform.position;
+            jointNode.parent = thePlayer.transform;
+            gameObject.transform.parent = jointNode;
 
-	if (fixedJoint!= null)
-		{
-			if (fixedJoint.connectedBody.gameObject.transform.localScale.x < 0)
-			{
-				Debug.Log("player turned");
-			}
-		}
+            fixedJoint = jointNode.gameObject.AddComponent<FixedJoint2D>();
+            fixedJoint.connectedBody = thePlayer.GetComponent<Rigidbody2D>();
+            fixedJoint.autoConfigureConnectedAnchor = false;
+            canPull = true;
+        }
+        else if (fixedJoint != null) {
+            Destroy(fixedJoint);
+            thePlayer = null;
+            canPull = false;
+            gameObject.transform.parent = null;
+            jointNode.parent = gameObject.transform;
+        }
+    }
 
-	}
+    // Update grounded state
+    IsGroundedCheck();
 
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if (other.gameObject.tag == "Player")
-		{
-			Debug.Log("Hit [E] to pull");
-			isTouchingPlayer = true;
-			thePlayer = other.gameObject;
-		}
-	}
+    // Check horizontal movement by collider position (any movement)
+    float deltaX = Mathf.Abs(crateBottom.position.x - lastColliderPos.x);
 
-	void OnTriggerExit2D(Collider2D other){
-		if (other.gameObject.tag == "Player")
-		{
-			isTouchingPlayer = false;
-		}
-	}
+    // Play drag sound if crate is moving while grounded
+    if (isGrounded && deltaX > 0.001f) {
+        PlayDragSound();
+    } else {
+        StopDragSound();
+    }
 
-	//Set the crate's velocity = zero OnCollisionExit, to prevent sliding:
-	void OnCollisionExit2D(Collision2D other){
-		if (IsGroundedCheck()){
-			gameObject.GetComponent<Rigidbody2D>().linearVelocity = Vector3.zero;
-			if ((other.gameObject.GetComponent<Rigidbody2D>() != null) && (other.gameObject.GetComponent<Rigidbody2D>().bodyType ==RigidbodyType2D.Dynamic)){
-				gameObject.GetComponent<Rigidbody2D>().linearVelocity = Vector3.zero;
-			}
-		}
-	}
-
-	public bool IsGroundedCheck() {
-            Collider2D groundCheck = Physics2D.OverlapCircle(crateBottom.position, groundRange, groundLayer);
-            if (groundCheck != null) {
-                  //Debug.Log("crate is trouching ground!");
-				  isGrounded = true;
-                  return true;
-            }
-			isGrounded=false;
-            return false;
-	}
-
-
-	void OnDrawGizmosSelected(){
-            Gizmos.DrawWireSphere(crateBottom.position, groundRange);
-      }
-
-
+    lastColliderPos = crateBottom.position; // store for next frame
 }
 
-/*
-	//check if touching ground:
-	void OnCollisionEnter2D(Collision2D other){
-		if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
-		//if (other.gameObject.layer == groundLayer)
-		{
-			
-		}
-	}
-*/
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            Debug.Log("Hit [E] to pull");
+            isTouchingPlayer = true;
+            thePlayer = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            isTouchingPlayer = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other) {
+        if (IsGroundedCheck()) {
+            //gameObject.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+        }
+    }
+
+    public bool IsGroundedCheck() {
+        Collider2D groundCheck = Physics2D.OverlapCircle(crateBottom.position, groundRange, groundLayer);
+        isGrounded = (groundCheck != null);
+        return isGrounded;
+    }
+
+    void PlayDragSound() {
+        dragTimer -= Time.deltaTime;
+        if (dragTimer <= 0f && dragSounds.Length > 0) {
+            int index = Random.Range(0, dragSounds.Length);
+            currentDragSound = dragSounds[index];
+            currentDragSound.pitch = Random.Range(minPitch, maxPitch);
+            currentDragSound.Play();
+            dragTimer = dragRepeatTime;
+        }
+    }
+
+    void StopDragSound() {
+        if (currentDragSound != null && currentDragSound.isPlaying) {
+            currentDragSound.Stop();
+        }
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.DrawWireSphere(crateBottom.position, groundRange);
+    }
+}
